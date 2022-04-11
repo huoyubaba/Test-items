19bccb0080c60f59fea83f3394ba37589a27a0a3
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2022-04-11 09:00:48 +0800

    Update build2.sh

--- build2.sh
+++ build2.sh
@@ -6,12 +6,18 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: build2.sh
+# File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
 # Modify default IP
 sed -i 's/192.168.1.1/10.1.10.1/g' package/base-files/files/bin/config_generate
+
+# Modify WiFi ON
+# sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+
+# Modify default SSID
+sed -i 's/ssid=OpenWrt/ssid=BlueFire/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # Modify hostname
 sed -i 's/OpenWrt/BlueFire/g' package/base-files/files/bin/config_generate
