744e25f466b55ed6592b21197226e2dc3d55595a
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2022-04-19 14:08:05 +0800

    Update build2.sh

--- build2.sh
+++ build2.sh
@@ -17,7 +17,7 @@ sed -i 's/192.168.1.1/10.1.10.1/g' package/base-files/files/bin/config_generate
 # sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # Modify default SSID
-# sed -i 's/ssid=OpenWrt/ssid=BlueFire/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+sed -i 's/ssid=OpenWrt/ssid=BlueFire/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # Modify hostname
 sed -i 's/OpenWrt/BlueFire/g' package/base-files/files/bin/config_generate
