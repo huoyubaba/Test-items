da793030473e53433cad5cd76ba5fd5e55851ac6
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2022-08-20 10:11:31 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -15,9 +15,6 @@
 
 # Add a feed source
 # sed -i '$a src-git kenzo https://github.com/kenzok8/small-package' feeds.conf.default   # 软件源
-# git clone https://github.com/erdoukki/luci-app-arpbind.git package/luci-app-arpbind
-# git clone https://github.com/zlg98/luci-app-vlmcsd.git package/luci-app-vlmcsd
-# git clone https://github.com/loryncien/luci-app-sqm.git package/luci-app-sqm
 git clone https://github.com/loryncien/luci-app-sqm.git package/luci-app-sqm
 git clone https://github.com/ricsc/sqm-scripts.git package/sqm-scripts
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-smartdns package/luci-app-smartdns #smartdns DNS加速
