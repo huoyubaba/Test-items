cde5f4698a5ab4ba02f0804dc818ebc526fe3d1a
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2022-08-20 11:45:06 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -32,7 +32,7 @@ svn co https://github.com/kenzok8/small-package/trunk/luci-app-ramfree package/l
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-usb3disable package/luci-app-usb3disable  #禁止USB3服务
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-accesscontrol package/luci-app-accesscontrol #上网时间
 svn co https://github.com/kenzok8/jell/trunk/luci-app-usb-printer package/luci-app-usb-printer  #共享打印服务
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-arpbind.git package/luci-app-arpbind   #IP/MAC绑定服务
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-arpbind package/luci-app-arpbind   #IP/MAC绑定服务
 svn co https://github.com/kenzok8/small-package/trunk/luci-app-oaf package/luci-app-oaf  #网站限制浏览服务
 svn co https://github.com/kenzok8/small-package/trunk/open-app-filter package/open-app-filter   #网站限制浏览服务
 svn co https://github.com/kenzok8/small-package/trunk/oaf package/oaf   #网站限制浏览服务
