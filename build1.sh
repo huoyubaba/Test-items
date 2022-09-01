178de629fd6a4cd3d0893e9e68522aa8cd15120d
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2022-09-01 08:20:21 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -21,7 +21,7 @@ svn co https://github.com/kenzok8/small-package/trunk/luci-app-smartdns package/
 svn co https://github.com/kenzok8/small-package/trunk/smartdns package/smartdns #smartdns DNS加速
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome #adguardhome 广告拦截 DNS加速
 svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome #adguardhome 广告拦截 DNS加速
-svn co https://github.com/kiddin9/openwrt-packages/trunk/applications/luci-app-vlmcsd package/luci-app-vlmcsd  #kms 激活服务
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd  #kms 激活服务
 svn co https://github.com/kiddin9/openwrt-packages/trunk/vlmcsd package/vlmcsd  #kms 激活服务
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-samba4 package/luci-app-samba4  #网络共享 服务器
 svn co https://github.com/kiddin9/openwrt-packages/trunk/autoshare-samba package/autoshare-samba  #网络共享 自动挂载服务
