b319da5bc0b9c988f690bfa9df591fd5dea1a5e8
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2022-10-13 09:47:30 +0800

    Create build3.sh

---
+++ build3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: build1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+# Add a feed source
+sed -i '$a src-git luci https://github.com/openwrt/luci' feeds.conf.default   # 软件源
+sed -i '$a src-git packages https://github.com/openwrt/packages' feeds.conf.default   # 基础包
