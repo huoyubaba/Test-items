0c851d48c321d20ffacefdacca3dcab685d14d7a
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2023-08-23 10:57:50 +0800

    Update build3.sh

--- build3.sh
+++ build3.sh
@@ -14,7 +14,7 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-sed -i '$a src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default  # 软件源
+# sed -i '$a src-git luci https://github.com/coolsnowwolf/luci' feeds.conf.default  # 软件源
 # sed -i '$a src-git packages https://github.com/openwrt/packages' feeds.conf.default   # 基础包
 # sed -i '$a src-git kenzo https://github.com/kenzok8/small-package' feeds.conf.default   # 软件源
 # git clone https://github.com/Plutonium141/luci-app-sqm.git package/luci-app-sqm #sqm流量整理
