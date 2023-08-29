97c20988fb197753dfc7f97101007625e2ddeea9
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2023-08-29 16:24:25 +0800

    Update build3.sh

--- build3.sh
+++ build3.sh
@@ -19,8 +19,8 @@
 # sed -i '$a src-git kiddin9 https://github.com/kiddin9/openwrt-packages' feeds.conf.default   # 软件源
 # git clone https://github.com/Plutonium141/luci-app-sqm.git package/luci-app-sqm #sqm流量整理
 git clone https://github.com/tohojo/sqm-scripts.git package/sqm-scripts #sqm流量整理
-svn co https://github.com/kenzok8/small-package/trunk/luci-app-smartdns package/luci-app-smartdns #smartdns DNS加速
-svn co https://github.com/kenzok8/small-package/trunk/smartdns package/smartdns #smartdns DNS加速
+git clone https://github.com/pymumu/openwrt-smartdns package/smartdns #smartdns DNS加速
+git clone https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns #smartdns DNS加速
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome #adguardhome 广告拦截 DNS加速
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome #adguardhome 广告拦截 DNS加速
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd  #kms 激活服务
