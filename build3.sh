05594fc8c5c1e2ad63adf0e30f22327303ed3e1e
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2023-09-02 09:37:16 +0800

    Update build3.sh

--- build3.sh
+++ build3.sh
@@ -39,11 +39,32 @@ svn co https://github.com/tcsr200722/luci-app-samba package/luci-app-samba  # �
 
 git clone https://github.com/ntlf9t/luci-app-easymesh.git package/luci-app-easymesh   # 简易联网
 
-svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-turboacc package/luci-app-turboacc   #网络加速附带插件
-svn co https://github.com/kiddin9/openwrt-packages/trunk/pdnsd-alt package/pdnsd-alt
-svn co https://github.com/kiddin9/openwrt-packages/trunk/dnsforwarder package/dnsforwarder
-svn co https://github.com/kiddin9/openwrt-packages/trunk/shortcut-fe package/shortcut-fe   #网络加速附带插件
+# svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-turboacc package/luci-app-turboacc   #网络加速附带插件
+# svn co https://github.com/kiddin9/openwrt-packages/trunk/pdnsd-alt package/pdnsd-alt
+# svn co https://github.com/kiddin9/openwrt-packages/trunk/dnsforwarder package/dnsforwarder
+# svn co https://github.com/kiddin9/openwrt-packages/trunk/shortcut-fe package/shortcut-fe   #网络加速附带插件
 
-# git clone https://github.com/holoto/luci-app-flowoffload.git package/luci-app-flowoffload
-
-# git clone https://github.com/whengrun/luci-app-turboacc.git package/luci-app-turboacc  #网络加速
+# Add truboacc source
+mkdir -p turboacc_tmp ./package/turboacc
+cd turboacc_tmp 
+git clone https://github.com/chenmozhijin/turboacc -b package
+cd ../package/turboacc
+git clone https://github.com/fullcone-nat-nftables/nft-fullcone
+git clone https://github.com/chenmozhijin/turboacc
+mv ./turboacc/luci-app-turboacc ./luci-app-turboacc
+rm -rf ./turboacc
+cd ../..
+cp -f turboacc_tmp/turboacc/hack-6.1/952-add-net-conntrack-events-support-multiple-registrant.patch ./target/linux/generic/hack-6.1/952-add-net-conntrack-events-support-multiple-registrant.patch
+cp -f turboacc_tmp/turboacc/hack-6.1/953-net-patch-linux-kernel-to-support-shortcut-fe.patch ./target/linux/generic/hack-6.1/953-net-patch-linux-kernel-to-support-shortcut-fe.patch
+cp -f turboacc_tmp/turboacc/pending-6.1/613-netfilter_optional_tcp_window_check.patch ./target/linux/generic/pending-6.1/613-netfilter_optional_tcp_window_check.patch
+rm -rf ./package/libs/libnftnl ./package/network/config/firewall4 ./package/network/utils/nftables
+mkdir -p ./package/network/config/firewall4 ./package/libs/libnftnl ./package/network/utils/nftables
+cp -r ./turboacc_tmp/turboacc/shortcut-fe ./package/turboacc
+cp -RT ./turboacc_tmp/turboacc/firewall4-$(grep -o 'FIREWALL4_VERSION=.*' ./turboacc_tmp/turboacc/version | cut -d '=' -f 2)/firewall4 ./package/network/config/firewall4
+cp -RT ./turboacc_tmp/turboacc/libnftnl-$(grep -o 'LIBNFTNL_VERSION=.*' ./turboacc_tmp/turboacc/version | cut -d '=' -f 2)/libnftnl ./package/libs/libnftnl
+cp -RT ./turboacc_tmp/turboacc/nftables-$(grep -o 'NFTABLES_VERSION=.*' ./turboacc_tmp/turboacc/version | cut -d '=' -f 2)/nftables ./package/network/utils/nftables
+rm -rf turboacc_tmp
+echo "# CONFIG_NF_CONNTRACK_CHAIN_EVENTS is not set" >> target/linux/generic/config-6.1
+echo "# CONFIG_SHORTCUT_FE is not set" >> target/linux/generic/config-6.1
+./scripts/feeds update -a
+./scripts/feeds install -a
