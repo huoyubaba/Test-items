653c6f6c279bc07547ec9f7709024a579d1b8319
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2023-09-27 20:12:02 +0800

    Update build3.sh

--- build3.sh
+++ build3.sh
@@ -22,7 +22,7 @@ git clone https://github.com/tohojo/sqm-scripts.git package/sqm-scripts #sqm流�
 
 #添加smartdns
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
-git clone -b lede https://github.com/pymumu/luci-app-smartdns.gi
+git clone -b lede https://github.com/pymumu/luci-app-smartdns.git
 
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome 广告拦截 DNS加速
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd  #kms 激活服务
