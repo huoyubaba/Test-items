b055f0f2930b6cc611e0663b8b7f4d830147bb3e
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2023-09-29 09:23:45 +0800

    Update build3.sh

--- build3.sh
+++ build3.sh
@@ -24,7 +24,7 @@ git clone https://github.com/tohojo/sqm-scripts.git package/sqm-scripts #sqm流�
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 
-# git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome 广告拦截 DNS加速
+git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome 广告拦截 DNS加速
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd  #kms 激活服务
 svn co https://github.com/kiddin9/openwrt-packages/trunk/vlmcsd package/vlmcsd  #kms 激活服务
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-samba4 package/luci-app-samba4  #网络共享 服务器
