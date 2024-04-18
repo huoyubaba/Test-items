31098b43977183db9487845e6a11703a2ea7428d
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2024-04-18 08:23:00 +0800

    Update build3.sh

--- build3.sh
+++ build3.sh
@@ -26,10 +26,10 @@ git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-a
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome # 广告拦截DNS加速
 
 svn co https://github.com/huoyubaba/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd  #kms激活服务
-svn co https://github.com/huoyubaba//openwrt-packages/trunk/vlmcsd package/vlmcsd  #kms激活服务
+svn co https://github.com/huoyubaba/openwrt-packages/trunk/vlmcsd package/vlmcsd  #kms激活服务
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-samba4 package/luci-app-samba4  #网络共享服务器
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/autoshare-samba package/autoshare-samba  #网络共享自动挂载服务
-svn co https://github.com/huoyubaba//openwrt-packages/trunk/luci-app-vsftpd package/luci-app-vsftpd  #网络ftp共享服务器
+svn co https://github.com/huoyubaba/openwrt-packages/trunk/luci-app-vsftpd package/luci-app-vsftpd  #网络ftp共享服务器
 svn co https://github.com/huoyubaba/openwrt-packages/trunk/vsftpd-alt package/vsftpd-alt  #网络共享ftp服务文件
 svn co https://github.com/huoyubaba/openwrt-packages/trunk/luci-app-arpbind package/luci-app-arpbind   #IP/MAC绑定服务
 svn co https://github.com/huoyubaba/openwrt-packages/trunk/luci-app-oaf package/luci-app-oaf  #控制访问页面
