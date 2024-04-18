930c901e4668ebe94701135b735191930db2313b
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2024-04-18 11:19:49 +0800

    Update build3.sh

--- build3.sh
+++ build3.sh
@@ -23,9 +23,9 @@ git clone https://github.com/tohojo/sqm-scripts.git package/sqm-scripts #sqm流�
 git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome # 广告拦截DNS加速
-
-svn co https://github.com/huoyubaba/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd  #kms激活服务
-svn co https://github.com/huoyubaba/openwrt-packages/trunk/vlmcsd package/vlmcsd  #kms激活服务
+git clone https://github.com/dgyangxf2023/luci-app-vlmcsd package/luci-app-vlmcsd
+# svn co https://github.com/huoyubaba/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd  #kms激活服务
+# svn co https://github.com/huoyubaba/openwrt-packages/trunk/vlmcsd package/vlmcsd  #kms激活服务
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-samba4 package/luci-app-samba4  #网络共享服务器
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/autoshare-samba package/autoshare-samba  #网络共享自动挂载服务
 svn co https://github.com/huoyubaba/openwrt-packages/trunk/luci-app-vsftpd package/luci-app-vsftpd  #网络ftp共享服务器
