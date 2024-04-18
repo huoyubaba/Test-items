f3c3d7da4e725e58f715ba23a26f8515e734c320
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2024-04-18 10:38:02 +0800

    Update build3.sh

--- build3.sh
+++ build3.sh
@@ -24,8 +24,8 @@ git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome # 广告拦截DNS加速
 
-svn co https://github.com/huoyubaba/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd  #kms激活服务
-svn co https://github.com/huoyubaba/openwrt-packages/trunk/vlmcsd package/vlmcsd  #kms激活服务
+svn co https://github.com/huoyubaba/openwrt-packages/branch/luci-app-vlmcsd package/luci-app-vlmcsd  #kms激活服务
+svn co https://github.com/huoyubaba/openwrt-packages/branch/vlmcsd package/vlmcsd  #kms激活服务
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-samba4 package/luci-app-samba4  #网络共享服务器
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/autoshare-samba package/autoshare-samba  #网络共享自动挂载服务
 svn co https://github.com/huoyubaba/openwrt-packages/trunk/luci-app-vsftpd package/luci-app-vsftpd  #网络ftp共享服务器
@@ -34,7 +34,7 @@ svn co https://github.com/huoyubaba/openwrt-packages/trunk/luci-app-arpbind pack
 svn co https://github.com/huoyubaba/openwrt-packages/trunk/luci-app-oaf package/luci-app-oaf  #控制访问页面
 svn co https://github.com/huoyubaba/openwrt-packages/trunk/open-app-filter package/open-app-filter   #控制访问页面
 svn co https://github.com/huoyubaba/openwrt-packages/trunk/oaf package/oaf   #控制访问页面
-svn co https://github.com/huoyubaba/openwrt-packages/trunk/luci-app-usb3disable package/luci-app-usb3disable  #禁用USB3.0
+svn co https://github.com/huoyubaba/openwrt-packages/branch/luci-app-usb3disable package/luci-app-usb3disable  #禁用USB3.0
 svn co https://github.com/huoyubaba/openwrt-packages/trunk/luci-app-usb-printer package/luci-app-usb-printer    #USB打印服务
 git clone https://github.com/tcsr200722/luci-app-samba package/luci-app-samba  # 网络共享服务3.6
 svn co https://github.com/huoyubaba/openwrt-packages/trunk/nftables package/nftables
