c91a7bc69c3aeb3249e1cd2c00b04d241a8a6ebc
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2024-04-18 08:19:27 +0800

    Update build3.sh

--- build3.sh
+++ build3.sh
@@ -25,17 +25,17 @@ git clone https://github.com/pymumu/openwrt-smartdns package/smartdns
 git clone -b lede https://github.com/pymumu/luci-app-smartdns.git package/luci-app-smartdns
 # git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome # 广告拦截DNS加速
 
-svn co https://github.com/kiddin9/openwrt-packages/tree/master/luci-app-vlmcsd package/luci-app-vlmcsd  #kms激活服务
-svn co https://github.com/kiddin9/openwrt-packages/tree/master/vlmcsd package/vlmcsd  #kms激活服务
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vlmcsd package/luci-app-vlmcsd  #kms激活服务
+svn co https://github.com/kiddin9/openwrt-packages/trunk/vlmcsd package/vlmcsd  #kms激活服务
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-samba4 package/luci-app-samba4  #网络共享服务器
 # svn co https://github.com/kiddin9/openwrt-packages/trunk/autoshare-samba package/autoshare-samba  #网络共享自动挂载服务
-svn co https://github.com/kiddin9/openwrt-packages/tree/master/luci-app-vsftpd package/luci-app-vsftpd  #网络ftp共享服务器
-svn co https://github.com/kiddin9/openwrt-packages/tree/master/vsftpd-alt package/vsftpd-alt  #网络共享ftp服务文件
-svn co https://github.com/kiddin9/openwrt-packages/tree/master/luci-app-arpbind package/luci-app-arpbind   #IP/MAC绑定服务
-svn co https://github.com/kiddin9/openwrt-packages/tree/master/luci-app-oaf package/luci-app-oaf  #控制访问页面
-svn co https://github.com/kiddin9/openwrt-packages/tree/master/open-app-filter package/open-app-filter   #控制访问页面
-svn co https://github.com/kiddin9/openwrt-packages/tree/master/oaf package/oaf   #控制访问页面
-svn co https://github.com/kiddin9/openwrt-packages/tree/master/luci-app-usb3disable package/luci-app-usb3disable  #禁用USB3.0
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-vsftpd package/luci-app-vsftpd  #网络ftp共享服务器
+svn co https://github.com/kiddin9/openwrt-packages/trunk/vsftpd-alt package/vsftpd-alt  #网络共享ftp服务文件
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-arpbind package/luci-app-arpbind   #IP/MAC绑定服务
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-oaf package/luci-app-oaf  #控制访问页面
+svn co https://github.com/kiddin9/openwrt-packages/trunk/open-app-filter package/open-app-filter   #控制访问页面
+svn co https://github.com/kiddin9/openwrt-packages/trunk/oaf package/oaf   #控制访问页面
+svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-usb3disable package/luci-app-usb3disable  #禁用USB3.0
 svn co https://github.com/kiddin9/openwrt-packages/trunk/luci-app-usb-printer package/luci-app-usb-printer    #USB打印服务
 svn co https://github.com/tcsr200722/luci-app-samba package/luci-app-samba  # 网络共享服务3.6
 svn co https://github.com/kiddin9/openwrt-packages/trunk/nftables package/nftables
