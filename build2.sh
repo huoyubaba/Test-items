a5b34b3142c180d7864e23807f5741786b6ecdd6
huoyubaba <94971430+huoyubaba@users.noreply.github.com>
2026-05-28 13:03:14 +0800

    Update build2.sh

--- build2.sh
+++ build2.sh
@@ -10,20 +10,27 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-# Modify default IP
+# 修改默认IP
 sed -i 's/192.168.1.1/10.1.10.1/g' package/base-files/files/bin/config_generate
 
-# Modify WiFi ON
-# sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+# 开机自动开启 WiFi
+sed -i 's/disabled=1/disabled=0/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# Modify default SSID
+# 修改 WiFi 名称
 sed -i 's/ssid=OpenWrt/ssid=BlueFire/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
-# Modify hostname
-sed -i 's/OpenWrt/‌BlueFire/g' package/base-files/files/bin/config_generate
+# 修改主机名
+sed -i 's/OpenWrt/BlueFire/g' package/base-files/files/bin/config_generate
 
-# 删除默认密码
-# sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
-# sed -i 's_downloads.openwrt.org_mirrors.tuna.tsinghua.edu.cn/openwrt_' /etc/opkg/distfeeds.conf
+# 移除冲突插件，防止编译失败
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
+rm -rf feeds/luci/applications/luci-app-openclash
 
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# 添加 helloworld 源（passwall/ssr++ 依赖）
+echo 'src-git helloworld https://github.com/fw876/helloworld' >> feeds.conf.default
+
+# 固件文件名自动加日期
+sed -i 's/IMG_PREFIX:=$(VERSION_DIST_SANITIZED)/IMG_PREFIX:=$(shell TZ=UTC-8 date "+%Y%m%d")-$(VERSION_DIST_SANITIZED)/g' include/image.mk
+
+# TTYD 免登录
+sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/config
